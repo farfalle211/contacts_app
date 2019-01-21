3d1657f9745300453a82e61c1772a036ecb4ae33
farfalle211 <farfalle211@gmail.com>
2019-01-21 09:44:22 -0600

    restful routes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,7 +1,47 @@
 class Api::ContactsController < ApplicationController
 
-  def contact_action
-    @contact = Contact.first
-    render 'contact_view.json.jbuilder'
+  def index
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
   end
+
+  def create
+    @contact = Contact.new(
+                            first_name: params[:first_name],
+                            last_name: params[:last_name],
+                            email: params[:email],
+                            phone_number: params[:phone_number]
+                          )
+    @contact.save
+    render 'show.json.jbuilder'
+
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find(contact_id)
+    render 'show.json.jbuilder'
+
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    @contact.first_name = params[:first_name] || @contact.first_name    #the "or" will keep it the same if you don't change it
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+
+    @contact.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+    render json: {message: "Successfully removed contact"}
+  end
+
+
+
 end
