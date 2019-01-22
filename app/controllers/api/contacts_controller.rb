c13282d23069ba196dd163a1771501860dd67d40
farfalle211 <farfalle211@gmail.com>
2019-01-22 11:00:26 -0600

    added bio and middle_name to update

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -10,8 +10,8 @@ class Api::ContactsController < ApplicationController
                             first_name: params[:first_name],
                             last_name: params[:last_name],
                             email: params[:email],
-                            phone_number: params[:phone_number]
-                            middle_name: params[:middle_name]
+                            phone_number: params[:phone_number],
+                            middle_name: params[:middle_name],
                             bio: params[:bio]
                           )
     @contact.save
@@ -30,9 +30,11 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
 
     @contact.first_name = params[:first_name] || @contact.first_name    #the "or" will keep it the same if you don't change it
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
 
     @contact.save
     render 'show.json.jbuilder'
